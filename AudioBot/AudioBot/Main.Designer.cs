760024bb64fa740840642ed0a4894def24e3d7ac
Chris Mortimer <Chris.Mortimer127654@hotmail.com>
2023-06-09 09:43:38 +0100

    Initial commit

---
+++ AudioBot/AudioBot/Main.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace AudioBot
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            joinChannel = new Button();
+            label2 = new Label();
+            channelDropdown = new ComboBox();
+            label1 = new Label();
+            audioDropdown = new ComboBox();
+            startStopAudio = new Button();
+            SuspendLayout();
+            // 
+            // joinChannel
+            // 
+            joinChannel.Location = new Point(180, 75);
+            joinChannel.Name = "joinChannel";
+            joinChannel.Size = new Size(68, 23);
+            joinChannel.TabIndex = 12;
+            joinChannel.Text = "Join";
+            joinChannel.UseVisualStyleBackColor = true;
+            joinChannel.Click += joinChannel_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 57);
+            label2.Name = "label2";
+            label2.Size = new Size(129, 15);
+            label2.TabIndex = 11;
+            label2.Text = "Discord channel to join";
+            // 
+            // channelDropdown
+            // 
+            channelDropdown.FormattingEnabled = true;
+            channelDropdown.Location = new Point(12, 75);
+            channelDropdown.Name = "channelDropdown";
+            channelDropdown.Size = new Size(162, 23);
+            channelDropdown.TabIndex = 10;
+            channelDropdown.SelectionChangeCommitted += channelDropdown_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 10);
+            label1.Name = "label1";
+            label1.Size = new Size(172, 15);
+            label1.TabIndex = 9;
+            label1.Text = "Audio output device to capture";
+            // 
+            // audioDropdown
+            // 
+            audioDropdown.FormattingEnabled = true;
+            audioDropdown.Location = new Point(12, 28);
+            audioDropdown.Name = "audioDropdown";
+            audioDropdown.Size = new Size(236, 23);
+            audioDropdown.TabIndex = 8;
+            audioDropdown.SelectedIndexChanged += audioDropdown_SelectedIndexChanged;
+            // 
+            // startStopAudio
+            // 
+            startStopAudio.Enabled = false;
+            startStopAudio.Location = new Point(12, 116);
+            startStopAudio.Name = "startStopAudio";
+            startStopAudio.Size = new Size(236, 30);
+            startStopAudio.TabIndex = 13;
+            startStopAudio.Text = "Start Audio";
+            startStopAudio.UseVisualStyleBackColor = true;
+            startStopAudio.Click += startStopAudio_Click;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(262, 157);
+            Controls.Add(startStopAudio);
+            Controls.Add(joinChannel);
+            Controls.Add(label2);
+            Controls.Add(channelDropdown);
+            Controls.Add(label1);
+            Controls.Add(audioDropdown);
+            Name = "Main";
+            Text = "AudioBot V1.0";
+            Load += Main_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button joinChannel;
+        private Label label2;
+        private ComboBox channelDropdown;
+        private Label label1;
+        private ComboBox audioDropdown;
+        private Button startStopAudio;
+    }
+}
---
+++ AudioBot/AudioBot/Main.cs
@@ -0,0 +1,204 @@
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using NAudio.CoreAudioApi;
+using NAudio.Gui;
+using NAudio.Wave;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Configuration;
+using System.Threading.Channels;
+using System.Windows.Input;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.CompilerServices;
+using Discord.Audio;
+using System.IO;
+using System.Text.RegularExpressions;
+using NAudio.Wave.SampleProviders;
+using System.Collections.Concurrent;
+
+namespace AudioBot
+{
+    public partial class Main : Form
+    {
+
+        private WasapiLoopbackCapture CaptureInstance;
+        readonly MMDeviceCollection devices = new MMDeviceEnumerator().EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active);
+        private MMDevice SelectedDevice;
+        private bool Recording = false;
+
+        string DISCORD_TOKEN = "YOUR_TOKEN_HERE";
+        private IAudioClient audioClient;
+        private bool JoinedChannel = false;
+        private IConfiguration _config;
+        private InteractionService _commands;
+        private DiscordSocketClient _client;
+        private List<IAudioChannel> Channels;
+        private IAudioChannel SelectedChannel;
+        private AudioOutStream AO;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Debug.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton(_config)
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+                .AddSingleton<CommandHandler>()
+                .BuildServiceProvider();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            audioDropdown.Items.Clear();
+
+            foreach (MMDevice device in devices)
+            {
+                try
+                {
+                    audioDropdown.Items.Add(device.FriendlyName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            var start = Start();
+            start.Wait();
+        }
+
+        private async Task Start()
+        {
+            var _builder = new ConfigurationBuilder();
+   
+            _config = _builder.Build();
+
+            var services = ConfigureServices();
+            var client = services.GetRequiredService<DiscordSocketClient>();
+            var commands = services.GetRequiredService<InteractionService>();
+            _client = client;
+            _commands = commands;
+
+            client.Log += Log;
+            commands.Log += Log;
+            client.Ready += Ready;
+
+            await client.LoginAsync(TokenType.Bot, DISCORD_TOKEN);
+            await client.StartAsync();
+
+            await services.GetRequiredService<CommandHandler>().InitializeAsync();
+        }
+
+        private async Task Ready()
+        {
+            var guilds = _client.Guilds;
+            Channels = new List<IAudioChannel>();
+
+            channelDropdown.Invoke((MethodInvoker)delegate
+            {
+                channelDropdown.Items.Clear();
+            });
+
+            foreach (SocketGuild guild in guilds)
+            {
+                foreach (SocketGuildChannel channel in guild.Channels)
+                {
+                    if (channel.GetChannelType() == ChannelType.Voice)
+                    {
+                        Channels.Add((IAudioChannel)channel);
+                        channelDropdown.Invoke((MethodInvoker)delegate
+                        {
+                            channelDropdown.Items.Add(channel);
+                        });
+                    }
+                }
+            }
+        }
+
+        private void startStopAudio_Click(object sender, EventArgs e)
+        {
+            if (!Recording)
+            {
+                this.CaptureInstance = new WasapiLoopbackCapture(SelectedDevice);
+                this.CaptureInstance.WaveFormat = new WaveFormat(48000, 16, 2);
+                this.CaptureInstance.ShareMode = AudioClientShareMode.Shared;
+                AO = audioClient.CreatePCMStream(AudioApplication.Voice);
+
+                this.CaptureInstance.DataAvailable += async (s, a) =>
+                {
+                    try
+                    {
+                        await AO.WriteAsync(a.Buffer, 0, a.BytesRecorded);
+                    }
+                    finally
+                    { }
+                };
+
+                this.CaptureInstance.RecordingStopped += (s, a) =>
+                {
+                    AO.Dispose();
+                    CaptureInstance.Dispose();
+                };
+
+                this.CaptureInstance.StartRecording();
+                Recording = true;
+                audioDropdown.Enabled = false;
+                startStopAudio.Text = "Stop Audio";
+            }
+            else
+            {
+                this.CaptureInstance.StopRecording();
+                Recording = false;
+                audioDropdown.Enabled = true;
+                startStopAudio.Text = "Start Audio";
+            }
+        }
+
+        private void joinChannel_Click(object sender, EventArgs e)
+        {
+            if (!JoinedChannel)
+            {
+                var s = SelectedChannel.ConnectAsync(false, false, false);
+                s.Wait();
+                audioClient = (IAudioClient)s.Result;
+                channelDropdown.Enabled = false;
+                joinChannel.Text = "Leave";
+                JoinedChannel = true;
+                startStopAudio.Enabled = true;
+            }
+            else
+            {
+                var s = SelectedChannel.DisconnectAsync();
+                s.Wait();
+                channelDropdown.Enabled = true;
+                joinChannel.Text = "Join";
+                JoinedChannel = false;
+                startStopAudio.Enabled = false;
+            }
+        }
+
+        private void channelDropdown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectedChannel = Channels[channelDropdown.SelectedIndex];
+        }
+
+        private void audioDropdown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectedDevice = devices[audioDropdown.SelectedIndex];
+        }
+    }
+}
